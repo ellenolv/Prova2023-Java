34bff4c36974da93a7ac32c426b7d6cd48ae1989
ellenolv <ellendeoliveirapereira@gmail.com>
2023-06-20 10:31:14 -0300

    1. Criar todos os códigos do Endpoint para cadastrar uma Empresa.

---
+++ src/main/java/com/fatec/prova_java/entities/Empresas.java
@@ -0,0 +1,50 @@
+package com.fatec.prova_java.entities;
+
+public class Empresas {
+    private int id;
+    private String razaoSocial;
+    private int number;
+    private boolean delivery;
+    private String cnpj;
+    private String endereco;
+
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+    public int getNumber() {
+        return number;
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    public boolean isDelivery() {
+        return delivery;
+    }
+    public void setDelivery(boolean delivery) {
+        this.delivery = delivery;
+    }
+    public String getCnpj() {
+        return cnpj;
+    }
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+
+}
--- src/main/java/com/fatec/prova_java/resources/cadastroController.java
+++ src/main/java/com/fatec/prova_java/resources/cadastroController.java
@@ -3,10 +3,22 @@ package com.fatec.prova_java.resources;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fatec.prova_java.entities.Empresas;
+
 @RestController
 public class cadastroController {
+
     @GetMapping("cadastro-empresa")
-    public String cadastroEmpresa(){
-        return "end point";
+    public Empresas cadastroEmpresa(){
+        Empresas e = new Empresas();
+
+        e.setId(1);
+        e.setRazaoSocial("SUBWAY DO BRASIL LTDA");
+        e.setCnpj("02.891.567/0001-20");
+        e.setDelivery(true);
+        e.setEndereco("Av. Dr. Ermelindo Maffei, 1199 - São Luiz, Itu - SP, 13304-305");
+        e.setNumber(15151515);
+     
+        return e;
     }
 }
